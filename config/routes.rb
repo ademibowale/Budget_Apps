213b46e4d1e711616e226a1b534d63d9a6c46ae8
adebowale <ademibowaleadegboye@gmail.com>
2022-11-17 19:59:48 +0100

    complete homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'users#index'
 
   resources :users, only: [:index] do
     resources :groups, only: %i[index new create show destroy] do
