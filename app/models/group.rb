0ec8fff38b41c68f00e84c3bd997ea0d577074d9
adebowale <ademibowaleadegboye@gmail.com>
2022-11-20 14:48:58 +0100

    verify route

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :expenses, dependent: :destroy
-  # has_one_attached :icon
+  has_one_attached :icon
 
   validates :name, presence: true, length: { maximum: 25 }
   validates :icon, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-
 Rails.application.routes.draw do
   root 'home#index'
   get "/home", to: "homes#index"
@@ -18,30 +17,3 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
 end
-
-
-# # frozen_string_literal: true
-
-# Rails.application.routes.draw do
-#   devise_for :users
-#   devise_scope :user do
-#     get '/users/sign_out' => 'devise/sessions#destroy'
-#     authenticated :user do
-#       root 'groups#index', as: :authenticated_root
-#     end
-#     unauthenticated do
-#       root 'users#index', as: :unauthenticated_root
-#     end
-#   end
-
-#   # root to  :"home#index"
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-#   # Defines the root path route ("/")
-
-#   resources :users, only: [:index] do
-#     resources :groups, only: %i[index new create show destroy] do
-#       resources :expenses, only: %i[index new create show destroy]
-#     end
-#   end
-# end
